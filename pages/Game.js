af0c1d0f1aa41c466766dbc04dc9845cf83693e0
Sina Rahimi <sinarahimi.main@gmail.com>
2023-05-28 00:07:45 +0200

    crypto cards improved in game page

--- pages/Game.js
+++ pages/Game.js
@@ -128,11 +128,12 @@ export default function Game () {
 
     return (
 
-        <div className="h-full bg-gradient-to-b from-rose-400 to-violet-900">
+        <div className="h-full bg-gradient-to-b from-rose-600 to-violet-500">
             <header className="bg-gray-800 text-white p-4">
-                <div className="flex justify-between">
+                <div className="flex justify-between items-center">
                     <Link href="./Hub"><h1 className="text-xl font-bold ml-5">KalKal</h1></Link>
-                    <div className="flex">
+                    <div className="flex items-center">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-3 rounded focus:outline-none focus:shadow-outline" onClick={startWrite}>Start</button>
                         <h2 className='pr-1'>Game Status:</h2>
                         {paused ? <p className="text-red-500">  Not started/Finished</p> : <p className="text-green-600">Ongoing</p>}
                     </div>
@@ -140,11 +141,7 @@ export default function Game () {
                 </div>
             </header>
             <div className="flex">
-                <div className="w-3/6 h-screen border rounded-md mr-0.5">
-                    <RecentPlayers contract={contract}></RecentPlayers>
-                </div>
-
-                <div className="w-3/6 h-screen rounded-md ml-0.5">
+                <div className="w-3/6 h-screen rounded-md">
                     {isLoading || startLoading || endLoading && 
                     <div class="flex justify-center items-center h-full">
                         <p class="text-center">Check your wallet</p>
@@ -159,28 +156,27 @@ export default function Game () {
 
                     {!isLoading & !isSuccess && 
                     <div>
-                        <Information></Information>
-
-                        <div className="h-1/4 flex justify-center items-center">
-                            <input type="text" placeholder="Enter your name" onChange={(x) => setName(x.target.value)}></input>
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" disabled={choice == null || !name} onClick={write}>Place bet</button>
-                        </div>
-
-                        <div className="flex flex-wrap w-full h-3/4">
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 0 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(0)}>BTC</button>
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 1 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(1)}>ETH</button>
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 2 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(2)}>LINK</button>
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 3 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(3)}>MATIC</button>
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 4 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(4)}>SAND</button>
-                            <button className={`w-28 mx-1 h-1/4 border rounded-3xl ${choice === 5 ? 'bg-blue-700' : 'bg-blue-500'} hover:bg-blue-700 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(5)}>SOL</button>
+                        <p className='text-center text-white my-2'>( 1 ) pick a coin</p>
+                        <div className="w-full mt-2 flex px-1.5">
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 0 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(0)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-1" src="./btc.png"></img></button>                            
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 1 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(1)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-0.5" src="./eth.png"></img></button>                            
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 2 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(2)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-0.5" src="./link.png"></img></button>                            
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 3 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(3)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-1" src="./matic.png"></img></button>                            
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 4 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(4)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-0.5" src="./sand.png"></img></button>                            
+                            <button className={`w-20 py-10 my-1 ml-1 rounded-3xl ${choice === 5 ? 'bg-blue-500' : 'bg-fuchsia-200'} hover:bg-blue-500 text-white font-bold focus:outline-none focus:shadow-outline`} onClick={() => setChoice(5)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><img className="px-1" src="./sol.png"></img></button>                            
                         </div> 
-                        <div className='flex justify-center items-center h-full'>
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={startWrite}>Start</button>
-                            {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={endWrite}>End</button> */}
+                        <p className='text-center text-white mb-1 mt-4'>( 2 ) enter your name and submit</p>
+                        <div className='my-2 mx-2 flex justify-center'>
+                            <input className="w-full h-10 rounded-md px-2" type="text" placeholder="Rahimjackass" onChange={(x) => setName(x.target.value)}></input>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm focus:outline-none focus:shadow-outline" disabled={choice == null || !name} onClick={write}>submit</button>
                         </div>
+                        {/* <Information></Information> */}
                     </div>
                     }
-                </div>                  
+                </div>     
+                <div className="w-4/6 h-screen border rounded-md">
+                    <RecentPlayers contract={contract}></RecentPlayers>
+                </div>             
             </div>
         </div>
   
